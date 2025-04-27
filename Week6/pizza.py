7140f01f719708ab80c647ca013278d07db7e1c9
JiuYuuu <jiuyuuu233@gmail.com>
2025-04-27 22:16:11 +0800

    Create pizza.py

---
+++ Week6/pizza.py
@@ -0,0 +1,20 @@
+import sys
+import csv
+from tabulate import tabulate
+
+if len(sys.argv) != 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+filename = sys.argv[1]
+if not filename.endswith(".csv"):
+    sys.exit("Not a CSV file")
+
+try:
+    with open(filename) as filename:
+        reader = csv.DictReader(filename)
+        data = list(reader)
+        print(tabulate(data, headers="keys", tablefmt="grid"))
+
+except FileNotFoundError:
+    sys.exit("File does not exist")
