dfe1daef24f56b7264d5c7671db8b83a5ef4bfad
JiuYuuu <jiuyuuu233@gmail.com>
2025-04-28 14:43:19 +0800

    Update lines.py

--- Week6/lines.py
+++ Week6/lines.py
@@ -1,6 +1,6 @@
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) == 1:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
